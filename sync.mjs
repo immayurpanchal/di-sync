eb3f6a8c400adca0de7ebaa13f3d63ae6aa2efe4
Mayur Panchal <mayur.panchal@deepintent.com>
2022-08-08 17:25:56 +0530

    refactor: :fire: remove sync.mjs
    using .zsh script instead

--- sync.mjs
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zx
-
-const branch = argv._[0];
-await $`git pull`;
-await $`git checkout ${branch}`;
-await $`git pull origin ${branch}`;
-await $`git checkout -`;
-await $`git merge ${branch}`;
