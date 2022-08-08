5bd13e5bab8a8415e031d025e5c03799ee25590f
Mayur Panchal <mayur.panchal@deepintent.com>
2022-08-08 17:26:30 +0530

    refactor: :recycle: renamed to .zsh
    move all mjs commands to .zsh

---
+++ sync.plugin.zsh
@@ -0,0 +1,18 @@
+echo "hi"
+
+# DeepIntent Branch Sync Plugin
+dis() {
+  branchToSyncWith="$1"
+  
+  if [ -z "$branchToSyncWith" ]; then
+    echo "Please Provide Branch Name"
+    echo "Example: dis master"
+    return 1
+  fi
+
+  echo "$(git pull)"
+  echo "$(git checkout "$branchToSyncWith")"
+  echo "$(git pull origin "$branchToSyncWith")"
+  echo "$(git checkout -)"
+  echo "$(git merge "$branchToSyncWith")"
+}
