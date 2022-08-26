03077c82150bbe9a86dcf22ac8fd67208b561725
Mayur Panchal <mayur.panchal@deepintent.com>
2022-08-26 11:08:10 +0530

    refactor: :recycle: refactor pr generator code into old file

--- sync.plugin.zsh
+++ sync.plugin.zsh
@@ -14,3 +14,34 @@ dis() {
   echo "$(git checkout -)"
   echo "$(git merge "$branchToSyncWith")"
 }
+
+# DeepIntent PR Generator plugin 
+
+dipr(){
+  prAgainstBranch="$1" # staging
+  currentBranch="$(git branch --show-current)" # feat/di-123_test 
+  projectName="$(git config --local remote.origin.url|sed -n 's#.*/\([^.]*\)\.git#\1#p')" # di-advertiser-ui
+
+  echo "current branch is : ${currentBranch}, PR against : ${prAgainstBranch}"
+
+  if [ -z "$prAgainstBranch" ]; then
+    echo "Please Provide Branch Name"
+    echo "Example: dipr staging"
+    return 1
+  fi
+
+  newBranch="${currentBranch}_${prAgainstBranch}" # feat/di-123_test_staging
+
+  echo "$(git checkout ${prAgainstBranch})" # git checkout staging 
+  echo "$(git pull)" # git pull 
+  echo "$(git checkout -b ${newBranch})" # git checkout -b feat/di-123_test_staging
+  echo "$(git merge ${currentBranch})"# git merge feat/di-123_test
+  echo "$(git push --set-upstream origin ${newBranch})" # git push 
+  
+  finalPRUrl="https://github.com/DeepIntent/${projectName}/compare/${prAgainstBranch}...${newBranch}" # https://github.com/immayurpanchal/di-sync/compare/staging...feat/di-123_test_staging
+  echo "-----------------------------------------"
+  echo "${finalPRUrl}" # open https://github.com/immayurpanchal/di-sync/compare/staging...feat/di-123_test_staging
+  
+  # echo "$(https://github.com/immayurpanchal/di-sync/pull/new/test)"
+  # https://github.com/immayurpanchal/di-sync/compare/master...test?expand=1
+}
